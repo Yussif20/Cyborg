c72affbd315334cebd08791a553f55b4fa51d633
Yousef <yousefayman2000500@gmail.com>
2023-01-30 12:08:55 +0200

    adding the react icons and importing the photos

--- src/sections/MostPopular/MostPopular.jsx
+++ src/sections/MostPopular/MostPopular.jsx
@@ -1,5 +1,12 @@
 import "./MostPopular.css";
 import imgOne from "../../assets/Images/popular-01.jpg";
+import imgTwo from "../../assets/Images/popular-02.jpg";
+import imgThree from "../../assets/Images/popular-03.jpg";
+import imgFour from "../../assets/Images/popular-04.jpg";
+import imgFive from "../../assets/Images/popular-05.jpg";
+import imgSix from "../../assets/Images/popular-06.jpg";
+import imgSeven from "../../assets/Images/popular-07.jpg";
+import imgEight from "../../assets/Images/popular-08.jpg";
 import Card from "../../components/Card/Card";
 
 const MostPopular = () => {
@@ -18,49 +25,49 @@ const MostPopular = () => {
             download="2.2M"
           />
           <Card
-            image={imgOne}
+            image={imgTwo}
             title="Pubg"
             category="Legendry"
             rate="4.8"
             download="3.5M"
           />
           <Card
-            image={imgOne}
+            image={imgThree}
             title="CS-Go"
             category="Stream-X"
             rate="4.5"
             download="1.2M"
           />
           <Card
-            image={imgOne}
+            image={imgFour}
             title="Witcher"
             category="Legendry"
             rate="4"
             download="3.4M"
           />
           <Card
-            image={imgOne}
+            image={imgFive}
             title="Fortnight"
             category="Sandbox"
             rate=""
             download=""
           />
           <Card
-            image={imgOne}
+            image={imgSix}
             title="Pubg"
             category="Legendry"
             rate=""
             download=""
           />
           <Card
-            image={imgOne}
+            image={imgSeven}
             title="CS-Go"
             category="Sandbox"
             rate=""
             download=""
           />
           <Card
-            image={imgOne}
+            image={imgEight}
             title="Witcher"
             category="Legendry"
             rate=""
