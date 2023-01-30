cb427a1a807057e129cf3eb27d397ad512d1f60a
Yousef <yousefayman2000500@gmail.com>
2023-01-30 14:16:50 +0200

    managing cards data with array map

---
+++ src/Data/MostPopularData.js
@@ -0,0 +1,76 @@
+import imgOne from "../assets/Images/popular-01.jpg";
+import imgTwo from "../assets/Images/popular-02.jpg";
+import imgThree from "../assets/Images/popular-03.jpg";
+import imgFour from "../assets/Images/popular-04.jpg";
+import imgFive from "../assets/Images/popular-05.jpg";
+import imgSix from "../assets/Images/popular-06.jpg";
+import imgSeven from "../assets/Images/popular-07.jpg";
+import imgEight from "../assets/Images/popular-08.jpg";
+
+const mostPopularData = [
+  {
+    id: "card_00",
+    image: imgOne,
+    title: "Fortnight",
+    category: "Sandbox",
+    rate: "4.2",
+    download: "2.2M",
+  },
+  {
+    id: "card_01",
+    image: imgTwo,
+    title: "Pubg",
+    category: "Legendry",
+    rate: "4.8",
+    download: "3.5M",
+  },
+  {
+    id: "card_02",
+    image: imgThree,
+    title: "CS-Go",
+    category: "Stream-X",
+    rate: "4.5",
+    download: "1.2M",
+  },
+  {
+    id: "card_03",
+    image: imgFour,
+    title: "Witcher",
+    category: "Legendry",
+    rate: "4",
+    download: "3.4M",
+  },
+  {
+    id: "card_04",
+    image: imgFive,
+    title: "Fortnight",
+    category: "Sandbox",
+    rate: "4.1",
+    download: "1.6M",
+  },
+  {
+    id: "card_05",
+    image: imgSix,
+    title: "Pubg",
+    category: "Legendry",
+    rate: "3.5",
+    download: "1M",
+  },
+  {
+    id: "card_06",
+    image: imgSeven,
+    title: "CS-Go",
+    category: "Sandbox",
+    rate: "4.1",
+    download: "1.2M",
+  },
+  {
+    id: "card_07",
+    image: imgEight,
+    title: "Witcher",
+    category: "Legendry",
+    rate: "3.9",
+    download: "5.6M",
+  },
+];
+export default mostPopularData;
--- src/sections/MostPopular/MostPopular.jsx
+++ src/sections/MostPopular/MostPopular.jsx
@@ -1,76 +1,25 @@
 import "./MostPopular.css";
-import imgOne from "../../assets/Images/popular-01.jpg";
-import imgTwo from "../../assets/Images/popular-02.jpg";
-import imgThree from "../../assets/Images/popular-03.jpg";
-import imgFour from "../../assets/Images/popular-04.jpg";
-import imgFive from "../../assets/Images/popular-05.jpg";
-import imgSix from "../../assets/Images/popular-06.jpg";
-import imgSeven from "../../assets/Images/popular-07.jpg";
-import imgEight from "../../assets/Images/popular-08.jpg";
+import mostPopularData from "../../Data/MostPopularData";
+
 import { Card, SectionHeader, SectionWrapper } from "../../components/index.js";
 const MostPopular = () => {
+  const cards = mostPopularData.map((card) => {
+    return (
+      <Card
+        key={card.id}
+        image={card.image}
+        title={card.title}
+        category={card.category}
+        rate={card.rate}
+        download={card.download}
+      />
+    );
+  });
   return (
     <>
       <SectionWrapper>
         <SectionHeader>Most popular</SectionHeader>
-        <div className="most-popular-items">
-          <Card
-            image={imgOne}
-            title="Fortnight"
-            category="Sandbox"
-            rate="4.2"
-            download="2.2M"
-          />
-          <Card
-            image={imgTwo}
-            title="Pubg"
-            category="Legendry"
-            rate="4.8"
-            download="3.5M"
-          />
-          <Card
-            image={imgThree}
-            title="CS-Go"
-            category="Stream-X"
-            rate="4.5"
-            download="1.2M"
-          />
-          <Card
-            image={imgFour}
-            title="Witcher"
-            category="Legendry"
-            rate="4"
-            download="3.4M"
-          />
-          <Card
-            image={imgFive}
-            title="Fortnight"
-            category="Sandbox"
-            rate="4.1"
-            download="1.6M"
-          />
-          <Card
-            image={imgSix}
-            title="Pubg"
-            category="Legendry"
-            rate="3.5"
-            download="1M"
-          />
-          <Card
-            image={imgSeven}
-            title="CS-Go"
-            category="Sandbox"
-            rate="4.1"
-            download="1.2M"
-          />
-          <Card
-            image={imgEight}
-            title="Witcher"
-            category="Legendry"
-            rate="3.9"
-            download="5.6M"
-          />
-        </div>
+        <div className="most-popular-items">{cards}</div>
       </SectionWrapper>
       ;
     </>
