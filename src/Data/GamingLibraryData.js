c01425f9b02e4d5eff03906949d2241721d178af
Yousef <yousefayman2000500@gmail.com>
2023-01-30 14:35:20 +0200

    adding the gaming library data

---
+++ src/Data/GamingLibraryData.js
@@ -0,0 +1,34 @@
+import gamingOne from "../assets/Images/game-01.jpg";
+import gamingTwo from "../assets/Images/game-02.jpg";
+import gamingThree from "../assets/Images/game-03.jpg";
+
+const gamingLibraryData = [
+  {
+    id: "game_00",
+    image: gamingOne,
+    title: "Fortnight",
+    category: "Sandbox",
+    data_added: "1/1/2040",
+    hours_played: "54 H 13 Mins",
+    downloaded: "downloaded",
+  },
+  {
+    id: "game_01",
+    image: gamingTwo,
+    title: "Pubg",
+    category: "Legendry",
+    data_added: "1/1/2040",
+    hours_played: "54 H 13 Mins",
+    downloaded: "download",
+  },
+  {
+    id: "game_02",
+    image: gamingThree,
+    title: "CS-Go",
+    category: "Stream-X",
+    data_added: "1/1/2040",
+    hours_played: "54 H 13 Mins",
+    downloaded: "downloaded",
+  },
+];
+export default gamingLibraryData;
